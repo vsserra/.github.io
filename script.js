1d0ec8582f12581592485800e5e99dd1b0c3bb59
vsserra <125168815+vsserra@users.noreply.github.com>
2023-02-12 11:08:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -88,3 +88,7 @@ document.getElementById("resultado").innerHTML = "AGUARDANDO SORTEIO...";
 document.getElementById("appBtn").addEventListener("click", function() {
     window.open("https://sorteiodejurados.online/sorteadordejurados.rar", "_blank");
   });
+
+ window.onload = function() {
+    document.getElementById("modal").style.display = "block";
+      }
